8e8c8389792230ce0c0fa8fded5a66a73ed8e02e
Stephen Touset <stephen@touset.org>
2008-05-13 11:31:11 -0400

    Tree somewhat implemented. Probably slow as crap, though.

--- lib/git/object/tree.rb
+++ lib/git/object/tree.rb
@@ -1,13 +1,12 @@
 require 'enumerator'
-require 'set'
 
 class Git::Object::Tree < Git::Object
-  INPUT_FORMAT = /^(\d+) (.+?)\0(.{20})/m
+  INPUT_FORMAT = /(\d+) (.+?)\0(.{20})/m
   
   MODE_MASK = 0777
   MODES     = {
     :tree   => 0040000,
-    :file   => 0100000,
+    :blob   => 0100000,
     :link   => 0120000,
     :commit => 0160000,
   }
@@ -15,36 +14,46 @@ class Git::Object::Tree < Git::Object
   attr_accessor :entries
   attr_accessor :metadata
   
-  def self.load(data)
-    tree = self.new()
-    
-    each_entry(data) do |mode, name, hash|
-      type = MODES.invert[mode & ~MODE_MASK]
-      mode = mode & MODE_MASK
-        
-      self.entries << Git::Object.find(hash)
-      self.metadata[hash] = { :name => name, :mode => mode }
-    end
-    
-    tree
-  end
-  
   def initialize()
-    self.entries  = Set.new
+    self.entries  = []
     self.metadata = {}
   end
   
-  def dump
+  def entries
+    @entries.map! do |e|
+      case e
+        when String then Git::Object.klass(metadata[e][:type]).find(e)
+        else             e
+      end
+    end
   end
   
   private
   
-  def self.each_entry(data, &blk)
-    fields = data.split(INPUT_FORMAT)
-    size   = (fields.length - 1) - fields.rindex('')
-    
+  def _dump
+    'blah'
+  end
+  
+  def _load(dump)
+    each_entry(dump) do |mode, name, hash|
+      type = MODES.invert[mode & ~MODE_MASK]
+      mode = mode & MODE_MASK
+      
+      @entries << hash
+      self.metadata[hash] = { :name => name, :type => type, :mode => mode }
+    end
+  end
+  
+  private
+  
+  def each_entry(dump)
+    fields = dump.split(INPUT_FORMAT)
     fields.reject! {|f| f.empty? }
     
-    fields.each_slice(size, &blk)
+    fields.enum_slice(3).each do |mode, name, hash|
+      mode = mode.to_i(8)             # mode is in octal
+      hash = hash.unpack('H40').first # hash is in binary format
+      yield(mode, name, hash)
+    end
   end
 end

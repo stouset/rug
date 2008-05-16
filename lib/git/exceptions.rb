e0c7469dc0b4d4aeec1c98a288ed3739f4653e80
Stephen Touset <stephen@touset.org>
2008-05-16 16:10:48 -0400

    rgit-show

--- lib/git/exceptions.rb
+++ lib/git/exceptions.rb
@@ -1,6 +1,8 @@
 module Git
   class StandardError      < ::StandardError; end
+  
   class CorruptLooseObject <   StandardError; end
   class InvalidTreeEntry   <   StandardError; end
   class ObjectTypeError    <   StandardError; end
+  class UnknownObjectError <   StandardError; end
 end

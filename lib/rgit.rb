b543860c3395c4b40bdb2aa420d54a44aa90e515
Stephen Touset <stephen@touset.org>
2008-05-28 19:49:29 -0400

    Application framework for rgit

---
+++ lib/rgit.rb
@@ -0,0 +1,26 @@
+require 'git'
+
+require 'optparse'
+require 'ostruct'
+require 'singleton'
+
+OpenStruct.class_eval { undef :id, :type }
+
+class Rgit
+  include Singleton
+  
+  def self.run!(args = ARGV)
+    self.instance.run!(args)
+  end
+  
+  def run!(args)
+    rest = parser.parse(args)
+    main(*rest)
+  rescue OptionParser::ParseError
+    puts parser
+  end
+  
+  def settings
+    OpenStruct.new
+  end
+end
---
+++ lib/rgit/hash-object.rb
@@ -0,0 +1,30 @@
+require 'rgit'
+
+class Rgit::HashObject < Rgit
+  def main(*files)
+    if settings.stdin
+      puts Git::Object.hash(settings.type, STDIN.read)
+    else
+      files.each do |name|
+        puts Git::Object.hash(settings.type, File.read(name))
+      end
+    end
+  end
+  
+  private
+  
+  def parser
+    OptionParser.new do |opts|
+      opts.banner = "Usage: rgit-hash-object [options] FILES..."
+      opts.on('-t', '--type TYPE', [:blob, :tree, :commit], 'a git object type') {|settings.type|}
+      opts.on('-s', '--stdin') {|settings.stdin|}
+    end
+  end
+  
+  def settings
+    @settings ||= OpenStruct.new(
+      :type  => 'blob',
+      :stdin => false
+    )
+  end
+end

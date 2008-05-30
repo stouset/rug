982a2ee8f9cf3cbf84cfa444449beee73c97aa90
Stephen Touset <stephen@touset.org>
2008-05-30 18:11:29 -0400

    rgit-cat-file

---
+++ lib/rgit/cat-file.rb
@@ -0,0 +1,55 @@
+require 'rgit'
+
+class Rgit::CatFile < Rgit
+  def main(*ids)
+    # we do this to match git's behavior
+    raise OptionParser::ParseError unless ids.length           == 1
+    raise OptionParser::ParseError unless settings.mode.length == 1
+    
+    id   = ids.shift
+    mode = settings.mode.shift
+    
+    # need to exit 1 if the object couldn't be found
+    object = Git::Object.find(id) rescue exit(1)
+    
+    case mode
+      when :type   then puts object.type
+      when :size   then puts object.dump.length
+      when :exists then nil
+      when :print  then puts object
+    end
+  end
+  
+  protected
+  
+  def parser
+    #
+    # TODO: support <type> like git-cat-file
+    #
+    OptionParser.new do |opts|
+      opts.banner = "Usage: rgit-cat-file [-t|-s|-e|-p] ID"
+      
+      opts.on('-t', '--type',
+        "print the object's type") \
+        { settings.mode << :type }
+      
+      opts.on('-s', '--size',
+        "print the object's size") \
+        { settings.mode << :size }
+      
+      opts.on('-e', '--exists',
+        'exit with zero status if the object exists' ) \
+        { settings.mode << :exists }
+      
+      opts.on('-p', '--print',
+        'pretty-print the object contents') \
+        { settings.mode << :print }
+    end
+  end
+  
+  def defaults
+    OpenStruct.new(
+      :mode  => []  # capture all listed modes to complain on multiples
+    )
+  end
+end

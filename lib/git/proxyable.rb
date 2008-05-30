64859503df1dc5b9b81284c775f1a83c2ea09eeb
Stephen Touset <stephen@touset.org>
2008-05-30 08:57:21 -0400

    Documentation

--- lib/git/proxyable.rb
+++ lib/git/proxyable.rb
@@ -4,13 +4,23 @@
 # avalanche effect, where loading one object causes all its children to load,
 # recusively.
 #
-# To defer loading an object, include Git::Proxyable, then wrap the
-# <tt>_load</tt> method in a <tt>proxy!</tt> block.
+# To defer loading an object, include Git::Proxyable, define your attributes
+# using attr_proxyable, then wrap your <tt>_load</tt> method in a
+# <tt>proxy!</tt> block.
 #
-#   def _load(dump)
-#     proxy!(dump) do
-#       # loading code
-#       ...
+#   class Git::Object::Foo < Git::Object
+#     include Git::Proxyable
+#
+#     attr_proxied :a
+#     attr_proxied :b
+#
+#     private
+#
+#     def _load(dump)
+#       proxy!(dump) do
+#         # assign a and b here
+#         ...
+#       end
 #     end
 #   end
 #
@@ -27,7 +37,12 @@ module Git::Proxyable
   end
   
   module ClassMethods
-    attr_accessor :proxied_attributes
+    #
+    # The list of proxied attributes.
+    #
+    def proxied_attributes
+      @proxied_attributes ||= []
+    end
     
     protected
     
@@ -35,9 +50,8 @@ module Git::Proxyable
     # Declare a proxyable attribute.
     #
     def attr_proxied(*attrs)
-      self.proxied_attributes ||= []
-      self.proxied_attributes  |= attrs
-      
+      self.proxied_attributes.push(attrs)
+      self.proxied_attributes.uniq!
       attr_accessor(*attrs)
     end
   end
@@ -48,6 +62,11 @@ module Git::Proxyable
   # Call this method to proxy all +proxied_attributes+. Takes the +dump+ of
   # the object's contents to load later, and a block that performs the actual
   # deferred load.
+  #
+  # Will rewrite the _inspect_ method to hide instance variables, the
+  # _dump method to return the exact contents of the +dump+ passed, and all
+  # +proxied_attributes+ to perform the load before returning their new
+  # contents.
   #
   def proxy!(dump, &loader)
     metaclass = class << self; self; end
@@ -65,7 +84,7 @@ module Git::Proxyable
       metaclass.send(:alias_method,  "proxied_#{a}=".to_sym, "#{a}=".to_sym)
       metaclass.send(:define_method, "#{a}".to_sym) { unproxy!; self.send(a) }
       metaclass.send(:define_method, "#{a}=".to_sym) do |v|
-        # only need to perform a load if we're not overwriting the thing
+        # only need to perform a load if we're not overwriting the whole thing
         unproxy!(self.class.proxied_attributes.length > 1)
         self.send("#{a}=".to_sym, v)
       end
@@ -76,6 +95,12 @@ module Git::Proxyable
   
   #
   # Forces the Object to finish loading, and unproxies all proxied attributes.
+  # 
+  # If +load+ is false, does not perform the deferred load. This might be
+  # preferred in the case of an object with one +proxied_attribute+ that's
+  # unproxied due to its attribute being overwritten. In such a case, there's
+  # no reason to parse its dump, since the contents will just be modified
+  # anyway.
   #
   def unproxy!(load = true)
     metaclass = class << self; self; end

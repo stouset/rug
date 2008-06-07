4e069cda49718fcee8cd729cbff34164b62aa257
Stephen Touset <stephen@touset.org>
2008-06-07 00:18:28 -0400

    Store and LooseObject using new repo-central paradigm

--- lib/git/store.rb
+++ lib/git/store.rb
@@ -1,17 +1,32 @@
-module Git::Store
-  def self.create(type, hash, dump)
-    Git::Store::LooseObject.create(type, hash, dump)
+class Git::Store
+  attr_reader :writer
+  attr_reader :readers
+  
+  def initialize(git_path)
+    self.writer   = Git::Store::LooseObject.new(git_path)
+    self.readers  = [ self.writer ]
   end
   
-  def self.find(hash)
-    begin
-      Git::Store::LooseObject.find(hash)
-    end or raise Git::ObjectNotFound, "not a valid object id '#{hash}'"
+  def get(id)
+    # TODO: avoid looking first, just try them all
+    readers.detect {|store| store.contains?(id) }.get(id)
+  rescue NoMethodError => e
+    raise e
+    raise Git::ObjectNotFound, "not a valid object id '#{id}'"
   end
   
-  def self.exists?(hash)
-    Git::Store::LooseObject.exists?(hash)
+  def put(id, type, dump)
+    writer.put(id, type, dump)
   end
+  
+  def contains?(id)
+    not readers.detect {|store| store.contains?(id) }.nil?
+  end
+    
+  private
+  
+  attr_writer :writer
+  attr_writer :readers
 end
 
 require 'git/store/loose_object'
--- lib/git/store/loose_object.rb
+++ lib/git/store/loose_object.rb
@@ -23,14 +23,17 @@ require 'zlib'
 # format are enumerated in VALID_OBJECTS.
 #
 class Git::Store::LooseObject
+  # location of the object store
+  OBJECT_PATH = 'objects'
+  
   # default permissions on loose objects
-  PERMS         = 0444
+  PERMS = 0444
   
   # length of the subdirectory names
   SUBDIR_LENGTH = 2
   
   # regexp to match loose object format
-  INPUT_FORMAT  = /^(\w+) (\d+)\0/
+  INPUT_FORMAT = /^(\w+) (\d+)\0/
   
   # sprintf-compatible output format
   OUTPUT_FORMAT = "%s %d\0%s"
@@ -38,16 +41,14 @@ class Git::Store::LooseObject
   # valid types for a loose object
   VALID_OBJECTS = [ :blob, :commit, :tree, :tag ]
   
-  attr_accessor :hash
-  attr_accessor :type
-  attr_accessor :dump
+  attr_accessor :path
   
-  #
-  # Instantiates and saves a new loose object, given the type of object, its
-  # hash, and the dump of its contents.
-  #
-  def self.create(hash, type, dump)
-    self.new(hash, type, dump).save
+  def initialize(git_path)
+    self.path = git_path.join(OBJECT_PATH)
+  end
+  
+  def contains?(id)
+    path_to(id).exists?
   end
   
   #
@@ -58,96 +59,43 @@ class Git::Store::LooseObject
   # Raises a CorruptLooseObject exception if the file contains any
   # discrepancies, such as an incorrect length or inappropriate type.
   #
-  def self.find(hash)
-    match = read(hash).match(INPUT_FORMAT)
+  def get(id)
+    match = read(id).match(INPUT_FORMAT)
     
     # extract the header and contents of the file
     type   = match[1].to_sym
     length = match[2].to_i
     dump   = match.post_match
     
-    verify_type(hash, type)
-    verify_length(hash, length, dump)
-    verify_hash(hash, type, dump)
+    verify_type(id, type)
+    verify_length(id, length, dump)
+    verify_id(id, type, dump)
     
-    self.new(hash, type, dump)
+    [type, dump]
   rescue Errno::ENOENT
     nil
   end
   
-  #
-  # Removes a loose object from disk by its hash identifier. Returns the hash
-  # if there was a file matching the hash and it was successfully removed.
-  # Returns nil if there was no file matching the hash identifier.
-  #
-  def self.destroy(hash)
-    path(hash).unlink
-    hash
-  rescue Errno::ENOENT
-    nil
-  end
-  
-  #
-  # Initializes a new loose object with the given object attributes. The type
-  # is the type of the git object, hash is the SHA-1 hash of its canonical
-  # form, and the dump is its raw dumped contents.
-  #
-  def initialize(hash, type, dump)
-    self.hash = hash
-    self.type = type.to_sym
-    self.dump = dump
-  end
-  
-  #
-  # Saves the contents of the object in a file on disk. Returns self. Raises
-  # a CorruptLooseObject exception if the contents don't pass all validations.
-  #
-  def save
-    unless saved?
+  def put(id, type, dump)
+    unless contains?(id)
       # TODO: decide whether or not I should perform validations like this at
-      # save-time, or only read-time. They can be quite slow.
-      self.class.verify_type(hash, type)
-      self.class.verify_length(hash, length, dump)
-      self.class.verify_hash(hash, type, dump)
+      # save-time, or only read-time; they may be slow.
+      verify_type(id, type)
+      verify_id(id, type, dump)
       
       contents = OUTPUT_FORMAT % [ type, length, dump ]
       
-      self.class.write(hash, contents)
+      write(id, contents)
     end
     
-    self
+    id
   end
   
-  #
-  # Removes the loose object's disk entry. Returns self.
-  #
-  def destroy
-    self.class.destroy(self.hash)
-    self
-  end
-  
-  #
-  # Returns whether or not there is already a file on disk whose contents
-  # match those of the current object.
-  #
-  # NOTE: Does NOT check to see if the file exists in other storage engines.
-  #
-  def saved?
-    self.class.exists?(self.hash)
-  end
-  
-  #
-  # Returns the filename of the loose object, based on its current contents.
-  #
-  def filename
-    self.class.path(hash)
-  end
-  
-  #
-  # The length of the object's contents
-  #
-  def length
-    dump.length
+  def delete(id)
+    path_to(id).unlink
+    id
+  rescue Errno::ENOENT
+    nil
   end
   
   private
@@ -155,8 +103,8 @@ class Git::Store::LooseObject
   #
   # Returns the entire contents of the file with the given hash.
   #
-  def self.read(hash)
-    Zlib::Inflate.inflate( path(hash).read )
+  def read(id)
+    Zlib::Inflate.inflate( path_to(id).read )
   end
   
   #
@@ -164,35 +112,24 @@ class Git::Store::LooseObject
   # and the +contents+ to write. BE CAREFUL. This method does not validate the
   # contents against the hash, so please be certain you're writing valid data.
   #
-  def self.write(hash, contents)
-    path = self.path(hash)
+  def write(id, contents)
+    path = path_to(id)
     path.dirname.mkpath
     path.open('w', PERMS) do |f|
       f.write Zlib::Deflate.deflate(contents)
     end
   end
   
-  #
-  # Returns the path associated with a given hash.
-  #
-  def self.path(hash)
-    name = hash.dup.insert(SUBDIR_LENGTH, File::SEPARATOR)
-    Git::Repository.object_path(name)
-  end
-  
-  #
-  # Checks whether or not a file exists with the given hash.
-  #
-  def self.exists?(hash)
-    path(hash).exist?
+  def path_to(id)
+    path.join id.dup.insert(SUBDIR_LENGTH, File::SEPARATOR)
   end
   
   #
   # Raises an exception if +type+ can't be contained in a loose object.
   #
-  def self.verify_type(hash, type)
+  def verify_type(id, type)
     unless VALID_OBJECTS.include? type
-      raise Git::CorruptLooseObject, "contents of #{hash} can't be a #{type}"
+      raise Git::CorruptLooseObject, "contents of #{id} can't be a #{type}"
     end
   end
   
@@ -200,19 +137,19 @@ class Git::Store::LooseObject
   # Raises an exception if the length of data doesn't match the specified
   # length.
   #
-  def self.verify_length(hash, length, dump)
+  def verify_length(id, length, dump)
     if length != dump.length
-      raise Git::CorruptLooseObject, "contents of #{hash} have bad length"
+      raise Git::CorruptLooseObject, "contents of #{id} have bad length"
     end
   end
   
   #
-  # Raises an exception if the hash of an object doesn't match the hash
+  # Raises an exception if the id of an object doesn't match the id
   # passed.
   #
-  def self.verify_hash(hash, type, dump)
-    if hash != Git::Object.hash(type, dump)
-      raise Git::CorruptLooseObject, "contents of #{hash} do not match hash"
+  def verify_id(id, type, dump)
+    if id != Git::Object.id(type, dump)
+      raise Git::CorruptLooseObject, "contents of #{id} do not match id"
     end
   end
 end

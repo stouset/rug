f931a7eca48b160ca53f7184710605253be41a54
Stephen Touset <stephen@touset.org>
2008-06-11 15:07:35 -0400

    Loading more files in git.rb

--- lib/git.rb
+++ lib/git.rb
@@ -4,8 +4,15 @@ end
 
 require 'core_ext/pathname'
 
-require 'git/author'
+# exceptions
 require 'git/exceptions'
+
+# auxiliary classes
+require 'git/author'
+require 'git/collection'
+require 'git/file'
+
+# main classes
 require 'git/repository'
 require 'git/store'
 require 'git/object'
